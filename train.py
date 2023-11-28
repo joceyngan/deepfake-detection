0dfbef38c74003bc7564f16bc712b97873d8f2de
Jocey <jocey.ngan@gmail.com>
2023-11-28 19:28:53 +0800

    fix allow training from scratch part

--- train.py
+++ train.py
@@ -42,11 +42,11 @@ config = {
 }
 
 if config["model_name"] == "vit_l_32":
-    model = models.vit_l_32(weights=models.ViT_L_32_Weights.DEFAULT)
+    model = models.vit_l_32(weights=models.ViT_L_32_Weights.DEFAULT if config["pretrained"] else None)
     num_features = model.heads.head.in_features
     model.heads.head = nn.Linear(num_features, 1)
 elif config["model_name"] == "swin_v2_b":
-    model = models.swin_v2_b(weights=models.Swin_V2_B_Weights.DEFAULT)
+    model = models.swin_v2_b(weights=models.Swin_V2_B_Weights.DEFAULT if config["pretrained"] else None)
     num_features = model.head.in_features
     model.head = nn.Linear(num_features, 1)
 elif config["model_name"] == "efficientnet_b0":
